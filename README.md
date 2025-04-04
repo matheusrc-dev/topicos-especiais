15f0ac3f91584a5de9417e89cbb27c481f179808
Matheus Rocha <matheusrochacamarg@gmail.com>
2025-04-04 20:40:42 -0300

    config: first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Repositório destinado à disciplina de Tópicos Especiais do curso de Tecnologia em Sistemas para Internet (TSI) da UTFPR.
+
---
+++ http-node-server/server.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "content-type": "text/html" });
+  res.write("<h1>Hello World</h1>");
+  res.write("<h2>Criando um servidor simples com Node.JS</h2>");
+  res.write("<p>Isso é incrível!</p>");
+  res.write(req.url);
+  res.write("<p>Agora está com Nodemon!</p>")
+  res.end();
+});
+
+server.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
+});
