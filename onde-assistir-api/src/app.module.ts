a6cfb8f11b805cf006445ce42d0e76ad07056707
Matheus Rocha <matheusrochacamarg@gmail.com>
2025-04-26 21:34:02 -0300

    chore: add shared and core module

--- onde-assistir-api/src/app.module.ts
+++ onde-assistir-api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
 
 @Module({
-  imports: [],
+  imports: [SharedModule, CoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
